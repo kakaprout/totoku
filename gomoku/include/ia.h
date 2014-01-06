ef547579299f3de7ec147253eb634a834597b741
suchet-q <quentin.suchet@epitech.eu>
2014-01-06 23:42:07 +0100

    final

--- gomoku/include/ia.h
+++ gomoku/include/ia.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <list>
+#include <array>
 #include "gomoku.h"
 
-#define INC(x, y) (x == y > ? 0 : x + 1)
-#define DEC(x, y) (x == 0 ? y : x - 1)
+#define INC(x, z, y) (x + z > y ? y : x + z)
+#define DEC(x, z) (x - z < 0 ? 0 : x - z)
 
 
 struct					t_node
@@ -12,35 +13,55 @@ struct					t_node
 	t_node				*parent;
 	char				x;
 	char				y;
+	char				map[19][19];
 	char				player;
 	int					weight;
-	std::list<t_node>	next;
+	std::list<t_node *>	next;
 };
 
 class Player_ia
 {
 public:
 	typedef		int	(Player_ia::*alignPtr)(t_node &to_ev, Gomoku *, int, int ,int);
-
+		typedef		int	(Player_ia::*blockPtr)(t_node &to_ev, Gomoku *, int, int ,int);
+	int			_eaten;
 	enum					e_action
 	{
 		ADD = 1,
-		CAPTURE = 10,
+		CAPTURE = 80,
 		BLOCK_1 = 2,
-		BLOCK_2 = 4,
-		BLOCK_3 = 8,
-		BLOCK_4 = 16,
+		BLOCK_2 = 5,
+		BLOCK_3 = 35,
+		BLOCK_4 = 90,
 		BLOCK_WIN = 1000,
 		COMPLETE_LINE_1 = 3,
-		COMPLETE_LINE_2 = 5,
-		COMPLETE_LINE_3 = 9,
-		COMPLETE_LINE_4 = 17,
+		COMPLETE_LINE_2 = 15,
+		COMPLETE_LINE_3 = 40,
+		COMPLETE_LINE_4 = 80,
 		WIN = 5000
 	};
 
 private:
+		t_node					*_nodePool;
+		int							_currentElem;
 		char numPlayer;
+		t_node		_start;
+		int			_buff;
+		t_node		*tmp;
+		std::list<t_node *>			_goodMoove;
 		alignPtr		*_alignPtr;
+		blockPtr		*_blockPtr;
+
+		inline int		checkBlock(t_node&, Gomoku*);
+		inline int		checkDownBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter);
+		inline int		checkTopBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter);
+		inline int		checkRightBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter);
+		inline int		checkLeftBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter);
+		inline int		checkTopLeftBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter);
+		inline int		checkTopRightBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter);
+		inline int		checkDownLeftBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter);
+	    inline int		checkDownRightBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter);
+		inline int		checkCapture(t_node &to_ev, Gomoku *g, int x, int y, int counter);
 
 		inline int		checkUpAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter);
 		inline int		checkDownAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter);
@@ -52,7 +73,13 @@ private:
 		inline int		checkDownRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter);
 
 		inline int		checkAlign(t_node&, Gomoku*);
-		int		evaluateHeuristic(t_node &to_evaluate, Gomoku*);
+
+		inline bool			checkIfPionInPerimetre(int x, int y, int rayon, Gomoku *g);
+		inline void			buildTree(Gomoku *g, int profondeur);
+		inline void			buildLevel(Gomoku *g, int profondeur, char player, t_node *daddy);
+		inline t_node*		getNode();
+		void				copyMap(char **src, char **dest);
+		int					evaluateHeuristic(t_node &to_evaluate, Gomoku*);
 public:
        Player_ia(char);
        ~Player_ia();
--- gomoku/src/ia.cpp
+++ gomoku/src/ia.cpp
@@ -3,6 +3,7 @@
 Player_ia::Player_ia(char player)
 {
 	this->numPlayer = player;
+	this->_nodePool = new t_node[500000];
 	this->_alignPtr = new alignPtr[8];
 	this->_alignPtr[0] = &Player_ia::checkUpAlign;
 	this->_alignPtr[1] = &Player_ia::checkDownAlign;
@@ -12,10 +13,427 @@ Player_ia::Player_ia(char player)
 	this->_alignPtr[5] = &Player_ia::checkUpLeftAlign;
 	this->_alignPtr[6] = &Player_ia::checkDownLeftAlign;
 	this->_alignPtr[7] = &Player_ia::checkDownRightAlign;
+	this->_blockPtr = new blockPtr[8];
+	this->_blockPtr[0] = &Player_ia::checkTopBlock;
+	this->_blockPtr[1] = &Player_ia::checkDownBlock;
+	this->_blockPtr[2] = &Player_ia::checkRightBlock;
+	this->_blockPtr[3] = &Player_ia::checkLeftBlock;
+	this->_blockPtr[4] = &Player_ia::checkTopLeftBlock;
+	this->_blockPtr[5] = &Player_ia::checkTopRightBlock;
+	this->_blockPtr[6] = &Player_ia::checkDownLeftBlock;
+	this->_blockPtr[7] = &Player_ia::checkDownRightBlock;
 }
 
 Player_ia::~Player_ia()
 {
+}
+
+t_node			*Player_ia::getNode()
+{
+	if (this->_nodePool[this->_currentElem].next.size())
+		this->_nodePool[this->_currentElem].next.clear();
+//	std::cout << this->_currentElem << std::endl;
+	return (&this->_nodePool[this->_currentElem++]);
+}
+
+int				Player_ia::checkDownRightBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter)
+{
+	bool check = false;
+	counter = 0;
+	x = to_ev.x + 1;
+	y = to_ev.y + 1;
+
+	while (y < 20 && x < 20 && g->c[y][x] != to_ev.player && g->c[y][x] > 0)
+	{
+		++counter;
+		++x;
+		++y;
+	}
+	if (x < 20 && y < 20 && g->c[y][x] == to_ev.player)
+		check = true;
+	if (counter == 2 && g->c[y][x] == to_ev.player)
+	{
+		this->_eaten += 2;
+		return CAPTURE;
+	}
+	switch (counter)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		if (check == true)
+			return (BLOCK_1 * 2);
+		return BLOCK_1;
+		break;
+	case 2:
+		if (check == true)
+			return (BLOCK_2 * 2);
+		return BLOCK_2;
+		break;
+	case 3:
+		if (check == true)
+			return (BLOCK_3 * 2);
+		return BLOCK_3;
+		break;
+	case 4:
+		if (check == true)
+			return (BLOCK_WIN);
+		return BLOCK_4;
+		break;
+	case 5:
+		return BLOCK_WIN;
+	}
+	return 0;
+}
+
+int				Player_ia::checkDownLeftBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter)
+{
+	bool check = false;
+	counter = 0;
+	x = to_ev.x - 1;
+	y = to_ev.y + 1;
+
+	while (y < 20 && x >= 0 && g->c[y][x] != to_ev.player && g->c[y][x] > 0)
+	{
+		++counter;
+		--x;
+		++y;
+	}
+	if (x >= 0 && y < 20 && g->c[y][x] == to_ev.player)
+		check = true;
+	if (counter == 2 && g->c[y][x] == to_ev.player)
+	{
+		this->_eaten += 2;
+		return CAPTURE;
+	}
+	switch (counter)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		if (check == true)
+			return (BLOCK_1 * 2);
+		return BLOCK_1;
+		break;
+	case 2:
+		if (check == true)
+			return (BLOCK_2 * 2);
+		return BLOCK_2;
+		break;
+	case 3:
+		if (check == true)
+			return (BLOCK_3 * 2);
+		return BLOCK_3;
+		break;
+	case 4:
+		if (check == true)
+			return (BLOCK_WIN);
+		return BLOCK_4;
+		break;
+	case 5:
+		return BLOCK_WIN;
+	}
+	return 0;
+}
+
+int				Player_ia::checkTopRightBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter)
+{
+	bool check = false;
+	counter = 0;
+	x = to_ev.x + 1;
+	y = to_ev.y - 1;
+
+	while (y >= 0 && x < 20 && g->c[y][x] != to_ev.player && g->c[y][x] > 0)
+	{
+		++counter;
+		++x;
+		--y;
+	}
+	if (x < 20 && y >= 0 && g->c[y][x] == to_ev.player)
+		check = true;
+	if (counter == 2 && g->c[y][x] == to_ev.player)
+	{
+		this->_eaten += 2;
+		return CAPTURE;
+	}
+	switch (counter)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		if (check == true)
+			return (BLOCK_1 * 2);
+		return BLOCK_1;
+		break;
+	case 2:
+		if (check == true)
+			return (BLOCK_2 * 2);
+		return BLOCK_2;
+		break;
+	case 3:
+		if (check == true)
+			return (BLOCK_3 * 2);
+		return BLOCK_3;
+		break;
+	case 4:
+		if (check == true)
+			return (BLOCK_WIN);
+		return BLOCK_4;
+		break;
+	case 5:
+		return BLOCK_WIN;
+	}
+	return 0;
+}
+
+int				Player_ia::checkTopLeftBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter)
+{
+	bool check = false;
+	counter = 0;
+	x = to_ev.x - 1;
+	y = to_ev.y - 1;
+
+	while (y >= 0 && x >= 0 && g->c[y][x] != to_ev.player && g->c[y][x] > 0)
+	{
+		++counter;
+		--x;
+		--y;
+	}
+	if (x >= 0 && y >= 0 && g->c[y][x] == to_ev.player)
+		check = true;
+	if (counter == 2 && g->c[y][x] == to_ev.player)
+	{
+		this->_eaten += 2;
+		return CAPTURE;
+	}
+	switch (counter)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		if (check == true)
+			return (BLOCK_1 * 2);
+		return BLOCK_1;
+		break;
+	case 2:
+		if (check == true)
+			return (BLOCK_2 * 2);
+		return BLOCK_2;
+		break;
+	case 3:
+		if (check == true)
+			return (BLOCK_3 * 2);
+		return BLOCK_3;
+		break;
+	case 4:
+		if (check == true)
+			return (BLOCK_WIN);
+		return BLOCK_4;
+		break;
+	case 5:
+		return BLOCK_WIN;
+	}
+	return 0;
+}
+
+int				Player_ia::checkLeftBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter)
+{
+	bool check = false;
+	counter = 0;
+	x = to_ev.x - 1;
+
+	while (x >= 0 && g->c[to_ev.y][x] != to_ev.player && g->c[to_ev.y][x] > 0)
+	{
+		++counter;
+		--x;
+	}
+	if (x >= 0 && g->c[to_ev.y][x] == to_ev.player)
+		check = true;
+	if (counter == 2 && g->c[to_ev.y][x] == to_ev.player)
+	{
+			this->_eaten += 2;
+		return CAPTURE;
+	}
+	switch (counter)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		if (check == true)
+			return (BLOCK_1 * 2);
+		return BLOCK_1;
+		break;
+	case 2:
+		if (check == true)
+			return (BLOCK_2 * 2);
+		return BLOCK_2;
+		break;
+	case 3:
+		if (check == true)
+			return (BLOCK_3 * 2);
+		return BLOCK_3;
+		break;
+	case 4:
+		if (check == true)
+			return (BLOCK_WIN);
+		return BLOCK_4;
+		break;
+	case 5:
+		return BLOCK_WIN;
+	}
+	return 0;
+}
+
+int				Player_ia::checkRightBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter)
+{
+	bool check = false;
+	counter = 0;
+	x = to_ev.x + 1;
+
+	while (x < 20 && g->c[to_ev.y][x] != to_ev.player && g->c[to_ev.y][x] > 0)
+	{
+		++counter;
+		++x;
+	}
+	if (x < 20 && g->c[to_ev.y][x] == to_ev.player)
+		check = true;
+	if (counter == 2 && g->c[to_ev.y][x] == to_ev.player)
+	{
+		this->_eaten += 2;
+		return CAPTURE;
+	}
+	switch (counter)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		if (check == true)
+			return (BLOCK_1 * 2);
+		return BLOCK_1;
+		break;
+	case 2:
+		if (check == true)
+			return (BLOCK_2 * 2);
+		return BLOCK_2;
+		break;
+	case 3:
+		if (check == true)
+			return (BLOCK_3 * 2);
+		return BLOCK_3;
+		break;
+	case 4:
+		if (check == true)
+			return (BLOCK_WIN);
+		return BLOCK_4;
+		break;
+	case 5:
+		return BLOCK_WIN;
+	}
+	return 0;
+}
+
+int				Player_ia::checkTopBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter)
+{
+	bool check = false;
+	counter = 0;
+	y = to_ev.y - 1;
+
+	while (y >= 0 && g->c[y][to_ev.x] != to_ev.player &&  g->c[y][to_ev.x] > 0)
+	{
+		++counter;
+		--y;
+	}
+	if (y >= 0 && g->c[y][to_ev.x] == to_ev.player)
+		check = true;
+	if (counter == 2 && g->c[y][to_ev.x] == to_ev.player)
+	{
+			this->_eaten += 2;
+		return CAPTURE;
+	}
+	switch (counter)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		if (check == true)
+			return (BLOCK_1 * 2);
+		return BLOCK_1;
+		break;
+	case 2:
+		if (check == true)
+			return (BLOCK_2 * 2);
+		return BLOCK_2;
+		break;
+	case 3:
+		if (check == true)
+			return (BLOCK_3 * 2);
+		return BLOCK_3;
+		break;
+	case 4:
+		if (check == true)
+			return (BLOCK_WIN);
+		return BLOCK_4;
+		break;
+	case 5:
+		return BLOCK_WIN;
+	}
+	return 0;
+}
+
+int				Player_ia::checkDownBlock(t_node &to_ev, Gomoku *g, int x, int y, int counter)
+{
+	bool check = false;
+	counter = 0;
+	y = to_ev.y + 1;
+
+	while (y < 20 && g->c[y][to_ev.x] != to_ev.player &&  g->c[y][to_ev.x] > 0)
+	{
+		++counter;
+		++y;
+	}
+	if (y < 20 && g->c[y][to_ev.x] == to_ev.player)
+		check = true;
+	if (counter == 2 && g->c[y][to_ev.x] == to_ev.player)
+	{
+			this->_eaten += 2;
+		return CAPTURE;
+	}
+	switch (counter)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		if (check == true)
+			return (BLOCK_1 * 2);
+		return BLOCK_1;
+		break;
+	case 2:
+		if (check == true)
+			return (BLOCK_2 * 2);
+		return BLOCK_2;
+		break;
+	case 3:
+		if (check == true)
+			return (BLOCK_3 * 2);
+		return BLOCK_3;
+		break;
+	case 4:
+		if (check == true)
+			return (BLOCK_WIN);
+		return BLOCK_4;
+		break;
+	case 5:
+		return BLOCK_WIN;
+	}
+	return 0;
 }
 
 int				Player_ia::checkDownAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter)
@@ -23,7 +441,7 @@ int				Player_ia::checkDownAlign(t_node &to_ev, Gomoku*g, int x, int y, int coun
 	counter = 0;
 	y = to_ev.y + 1;
 
-	while (y < 20 && g->c[y][to_ev.x] == to_ev.player)
+	while (y < 19 && g->c[y][to_ev.x] == to_ev.player)
 	{
 		++counter;
 		++y;
@@ -49,6 +467,7 @@ int				Player_ia::checkDownAlign(t_node &to_ev, Gomoku*g, int x, int y, int coun
 		return WIN;
 		break;
 	}
+	return 0;
 }
 
 int				Player_ia::checkUpAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter)
@@ -82,6 +501,7 @@ int				Player_ia::checkUpAlign(t_node &to_ev, Gomoku*g, int x, int y, int counte
 		return WIN;
 		break;
 	}
+	return 0;
 }
 
 int				Player_ia::checkLeftAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter)
@@ -115,6 +535,7 @@ int				Player_ia::checkLeftAlign(t_node &to_ev, Gomoku*g, int x, int y, int coun
 		return WIN;
 		break;
 	}
+	return (0);
 }
 
 int				Player_ia::checkRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter)
@@ -122,7 +543,7 @@ int				Player_ia::checkRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int cou
 	counter = 0;
 	x = to_ev.x + 1;
 
-	while (x < 20 && g->c[to_ev.y][x] == to_ev.player)
+	while (x < 19 && g->c[to_ev.y][x] == to_ev.player)
 	{
 		++counter;
 		++x;
@@ -148,6 +569,7 @@ int				Player_ia::checkRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int cou
 		return WIN;
 		break;
 	}
+	return 0;
 }
 
 int				Player_ia::checkUpLeftAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter)
@@ -183,6 +605,7 @@ int				Player_ia::checkUpLeftAlign(t_node &to_ev, Gomoku*g, int x, int y, int co
 		return WIN;
 		break;
 	}
+	return 0;
 }
 
 int				Player_ia::checkUpRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter)
@@ -191,7 +614,7 @@ int				Player_ia::checkUpRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int c
 	x = to_ev.x + 1;
 	y = to_ev.y - 1;
 
-	while (x < 20 && y >= 0 && g->c[y][x] == to_ev.player)
+	while (x < 19 && y >= 0 && g->c[y][x] == to_ev.player)
 	{
 		++counter;
 		++x;
@@ -218,6 +641,7 @@ int				Player_ia::checkUpRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int c
 		return WIN;
 		break;
 	}
+	return 0;
 }
 
 int				Player_ia::checkDownLeftAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter)
@@ -226,7 +650,7 @@ int				Player_ia::checkDownLeftAlign(t_node &to_ev, Gomoku*g, int x, int y, int 
 	x = to_ev.x - 1;
 	y = to_ev.y + 1;
 
-	while (x >= 0 && y < 20 && g->c[y][x] == to_ev.player)
+	while (x >= 0 && y < 19 && g->c[y][x] == to_ev.player)
 	{
 		++counter;
 		--x;
@@ -253,6 +677,7 @@ int				Player_ia::checkDownLeftAlign(t_node &to_ev, Gomoku*g, int x, int y, int 
 		return WIN;
 		break;
 	}
+	return 0;
 }
 
 int				Player_ia::checkDownRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int counter)
@@ -261,12 +686,13 @@ int				Player_ia::checkDownRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int
 	x = to_ev.x + 1;
 	y = to_ev.y + 1;
 
-	while (x < 20 && y < 20 && g->c[y][x] == to_ev.player)
+	while (x < 19 && y < 19 && g->c[y][x] == to_ev.player)
 	{
 		++counter;
 		++x;
 		++y;
 	}
+
 	switch (counter)
 	{
 	case 0:
@@ -288,31 +714,142 @@ int				Player_ia::checkDownRightAlign(t_node &to_ev, Gomoku*g, int x, int y, int
 		return WIN;
 		break;
 	}
+	return 0;
 }
 
 int				Player_ia::checkAlign(t_node &to_ev, Gomoku *g)
 {
 	int			ret = 0;
-	int			x;
-	int			y;
-	int			counter;
+	int			x = 0;
+	int			y = 0;
+	int			counter = 0;
 
 	for (int i = 0; i < 8; ++i)
+	{
 		ret += (this->*_alignPtr[i])(to_ev, g, x, y, counter);
+	}
+	return ret;
+}
+
+int				Player_ia::checkBlock(t_node &to_ev, Gomoku *g)
+{
+	int			ret = 0;
+	int			x = 0;
+	int			y = 0;
+	int			counter = 0;
+
+	for (int i = 0; i < 8; ++i)
+	{
+		ret += (this->*_blockPtr[i])(to_ev, g, x, y, counter);
+	}
 	return ret;
 }
 
 int				Player_ia::evaluateHeuristic(t_node &to_ev, Gomoku *g)
 {
-	int			ret;
+	int			ret = 0;
 
 	ret += checkAlign(to_ev, g);
 	ret += checkBlock(to_ev, g);
-
 	return ret;
+}
+
+bool			Player_ia::checkIfPionInPerimetre(int x, int y, int rayon, Gomoku *g)
+{
+	int x_min = DEC(x, rayon);
+	int x_max = INC(x, rayon, 19);
+	int y_min = DEC(y, rayon);
+	int y_max = INC(y, rayon, 19);
+
+	for (; y_min <= y_max; ++y_min)
+		for (x = x_min; x <= x_max; ++x)
+			if (g->c[y_min][x])
+				return true;
+	return false;
+}
+
+void			Player_ia::buildLevel(Gomoku *g, int profondeur, char player, t_node *daddy)
+{
+//	std::cout << "coucou" << std::endl;
+	for (int y = 0; y < 19; ++y)
+	{
+		for (int x = 0; x < 19; ++x)
+		{
+			if (/*checkIfPionInPerimetre(x, y, 1, g) &&*/ !g->c[y][x] /*g->check_case(x, y, player)*/)
+			{
+				this->_start.player = player;
+				this->_start.x = x;
+				this->_start.y = y;
+				if ((_buff = this->evaluateHeuristic(this->_start, g)) > 0)
+				{
+					tmp = this->getNode();
+					if (player != this->numPlayer)
+						this->_buff *= -1;
+					tmp->weight = _buff + daddy->weight;
+					tmp->parent = daddy;
+					tmp->y = y;
+					tmp->x = x;
+					copyMap((char **)daddy->map, (char **)tmp->map);
+					tmp->map[y][x] = player;
+					tmp->player = player;
+					daddy->next.push_back(tmp);
+					if (tmp->weight > 0)
+						this->_goodMoove.push_back(tmp);
+				}
+			}
+		}
+	}
+	for (std::list<t_node *>::iterator it = daddy->next.begin(); it != daddy->next.end(); ++it)
+	{	
+		if ((profondeur - 1))
+			this->buildLevel(g, profondeur - 1, player == 1 ? 2 : 1, (*it));
+	}
+}
+
+void			Player_ia::buildTree(Gomoku *g, int profondeur)
+{
+	buildLevel(g, profondeur, this->numPlayer, &this->_start);
+}
+
+void			Player_ia::copyMap(char **src, char **dest)
+{
+/*	for (int y = 0; y < 19; ++y)
+		for (int x = 0; x < 19; ++x)
+			dest[y][x] = src[y][x];*/
 }
 
 void			Player_ia::action(Gomoku *g, char *coord)
 {
-	//TODO
-}
+	_currentElem = 0;
+	copyMap((char **)g->c, (char **)this->_start.map);
+	this->_start.weight = 0;
+	buildTree(g, 2);
+	if (this->_start.next.size())
+	{
+		this->tmp = *(this->_goodMoove.begin());
+		for (std::list<t_node *>::iterator it = this->_goodMoove.begin(); it != this->_goodMoove.end(); ++it)
+		{
+			if ((*it)->weight > this->tmp->weight && g->check_case((*it)->x, (*it)->y, this->numPlayer))
+				this->tmp = (*it);
+		}
+		coord[0] = this->tmp->x;
+		coord[1] = this->tmp->y;
+	}
+	else
+	{
+		for (int y = 0; y < 19; ++y)
+			for (int x = 0; x < 19; ++x)
+			{
+				if (g->c[y][x + 1])
+				{
+					coord[0] = x;
+					coord[1] = y;
+					x = 19;
+					y = 19;
+				}
+			}
+	}
+	std::cout << this->_currentElem << std::endl;
+	this->_start.next.clear();
+	this->_goodMoove.clear();
+}	
